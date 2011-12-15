9c12d756007619b023cf0454fd1c9ea29d65c76e
Eric <ericroberts@gmail.com>
2011-12-15 17:11:28 -0500

    Saving items locally, adding canvas with background image removal

--- js/editor.js
+++ js/editor.js
@@ -4,48 +4,12 @@ $(function() {
   
   var scale_one = Math.abs((editor.window.height-100)/editor.defaults.height);
   var scale_two = Math.abs((editor.window.width-100)/editor.defaults.width);
-  
-  var zoom = Math.min(scale_one,scale_two);
-  
-  if(zoom > 1) {
-    zoom = 1;
-  } 
+  editor.window.zoom = Math.min(scale_one,scale_two,1);
   
   $.get("/boards/1.txt", function(data) {
     editor.currentboard = data;
     $.each(data.olioboard.object, function(i, object) {
-      
-      var baseX = editor.window.width/2;
-      var baseY = editor.window.height/2;
-      
-      // Create image and set styles using image data
-      var obj = document.createElement('div');
-      obj.setAttribute('id', object.id);
-      obj.setAttribute('class', 'object');
-      
-      var img = $('<img src="http://items.olioboard.com.s3.amazonaws.com/' + object.id + '_400x400.jpg" />');
-      img.css({
-        width: 400*object.media.scale*zoom,
-        height: 400*object.media.scale*zoom,
-        top: -(400*object.media.scale*zoom)/2,
-        left: -(400*object.media.scale*zoom)/2
-      });
-      
-      $(obj).css({
-        left: baseX + object.x*zoom,
-        top: baseY + object.y*zoom,
-        webkitTransform: "rotate("+object.media.rotation+"deg)",
-      }).data("object",object).append(img);
-      
-      // Add touch events
-      obj.addEventListener('touchmove', editor.touchmove, false);
-      obj.addEventListener('gesturestart', editor.gesture, false);
-      obj.addEventListener('gesturechange', editor.gesturechange, false);
-      obj.addEventListener('gestureend', editor.gestureend, false);
-      obj.addEventListener('touchstart', editor.touch, false);
-
-      // Finally, append the image to the canvas
-      $("#canvas").append(obj);
+      editor.addObject(object);
     });
   },"json");
   
@@ -56,21 +20,76 @@ $(window).resize(function() {
     width: $(window).width(),
     height: $(window).height()
   };
-  /*
-  $("#canvas img").each(function() {
-    var baseX = editor.window.width/2;
-    var baseY = editor.window.height/2;
-    var object = $(this).data("object");
-    
-    $(this).css({
-      left: baseX + object.x - (400*object.media.scale)/2,
-      top: baseY + object.y - (400*object.media.scale)/2
-    });
-  });
-  */
 });
 
 var editor = {
+  addObject: function(object) {
+    var base = {
+      x: editor.window.width/2,
+      y: editor.window.height/2
+    }
+    
+    var img = new Image(),
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        src = '/images/items/'+object.id+'_400x400.jpg';
+  
+    img.onload = function() {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.width = img.width;
+      ctx.height = img.height;
+      ctx.drawImage(img, 0, 0, img.width, img.height);
+      
+      // Remove the background if required
+      object.media.removed = "true";
+      if(object.media.removed == "true") {
+        var imageData = ctx.getImageData(0,0,img.width,img.height);
+        var pixel = imageData.data;
+        var r = 0, g = 1, b = 2, a = 3;
+        
+        /*
+        swatchMin = {
+          r: ,
+          g: ,
+          b: ,
+          a: 
+        }
+        */
+        for(var p = 0; p < pixel.length; p+=4) {
+          if((pixel[p+r] >= 250 && pixel[p+r] <= 255) && (pixel[p+g] >= 250 && pixel[p+g] <= 255) && (pixel[p+b] >= 250 && pixel[p+b] <= 255)) {
+            pixel[p+a] = 0;
+          }
+        }
+        ctx.putImageData(imageData,0,0);
+      }
+      
+      $(canvas).css({
+        width: 400*object.media.scale*editor.window.zoom,
+        height: 400*object.media.scale*editor.window.zoom,
+        top: base.y + object.y*editor.window.zoom,
+        left: base.x + object.x*editor.window.zoom,
+        webkitTransform: "rotate("+object.media.rotation+"deg)"
+      });
+      
+      $(canvas).css({
+        marginTop: -$(canvas).height()/2,
+        marginLeft: -$(canvas).width()/2
+      });
+      
+      // Add touch events
+      canvas.addEventListener('touchmove', editor.touchmove, false);
+      canvas.addEventListener('gesturestart', editor.gesture, false);
+      canvas.addEventListener('gesturechange', editor.gesturechange, false);
+      canvas.addEventListener('gestureend', editor.gestureend, false);
+      canvas.addEventListener('touchstart', editor.touch, false);
+      
+      document.getElementById('canvas').appendChild(canvas);
+    
+    }
+    
+    img.src = src;
+  },
   defaults: {
     width: 1960,
     height: 1440
@@ -84,15 +103,29 @@ var editor = {
     width: $(window).width(),
     height: $(window).height()
   },
+  /*
+  touchstart: function(event) {
+    if(!$(this).hasClass("object"))
+    $(".object").removeClass("active");
+  },
+  */
   touch: function(event) {
     var touch = event.changedTouches[0];
-    $(".object").removeClass("active");
-    $(this).addClass("active");
-    if(!editor.events.dragging){
-      editor.events.dragging = [touch.pageX - parseInt(this.style.left), touch.pageY - parseInt(this.style.top)];
+    
+    if($(this).hasClass("active")) {
+      if(!editor.events.dragging){
+        editor.events.dragging = [touch.pageX - parseInt(this.style.left), touch.pageY - parseInt(this.style.top)];
+      }
+    } else {
+      $(".active").removeClass("active");
+      $(this).addClass("target");
+      if(!editor.events.dragging){
+        editor.events.dragging = [touch.pageX - parseInt(this.style.left), touch.pageY - parseInt(this.style.top)];
+      }
     }
   },
   touchend: function(evt) {
+    $(".target").removeClass("target").addClass("active");
     editor.events.dragging = false;
   },
   touchmove: function(event) {
@@ -108,17 +141,17 @@ var editor = {
   },
   gesture: function(event) {
     if($(this).hasClass("active")) {
-      editor.events.sizing = [parseInt($(this).find("img").width()), parseInt($(this).find("img").height())];
+      editor.events.sizing = [parseInt($(this).width()), parseInt($(this).height())];
     }
   },
   gesturechange: function(event) {
     if($(this).hasClass("active")) {
       if(editor.events.sizing){
-        $(this).find("img").width(Math.min(editor.events.sizing[0] * event.scale, 600) + "px")
+        $(this).width(Math.min(editor.events.sizing[0] * event.scale, 600) + "px")
           .height(Math.min(editor.events.sizing[1] * event.scale, 600) + "px")
           .css({
-            top: -$(this).find("img").height()/2,
-            left: -$(this).find("img").width()/2
+            marginTop: -$(this).width()/2,
+            marginLeft: -$(this).height()/2,
           });
         this.style.webkitTransform = "rotate(" + ((editor.events.rotating + event.rotation) % 360) + "deg)";
       }
